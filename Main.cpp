52ee880f7a572c10d24e4d79d42a9998192d867d
Peng <peng.liu916@gmail.com>
2016-10-30 21:00:50 -0500

    Project Setup

---
+++ Main.cpp
@@ -0,0 +1,8 @@
+#include "pch.h"
+
+_Use_decl_annotations_
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+    return 0;
+}
---
+++ pch.cpp
@@ -0,0 +1,2 @@
+#include "pch.h"
+#pragma message("Compiling TSDFVolume Precompiled Headers")
